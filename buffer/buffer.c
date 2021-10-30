2ceb3859d95c89b6a39a66e4beab368404d8c06b
tashi <z28@umbc.edu>
2021-10-29 22:22:26 -0400

    buffer

---
+++ buffer/buffer.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <errno.h>
+
--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -1371,4 +1371,7 @@ int __sys_getsockopt(int fd, int level, int optname, char __user *optval,
 		int __user *optlen);
 int __sys_setsockopt(int fd, int level, int optname, char __user *optval,
 		int optlen);
+
+asmlinkage long sys_hello(void);
+
 #endif
